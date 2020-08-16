dc01f4ef8f808f6d984aec55a888d556235ca38f
Marshyy <28839897+Marshyy@users.noreply.github.com>
2020-08-16 21:34:33 +0530

    Delete invite.js

--- invite.js
+++
@@ -1,9 +0,0 @@
-function invite(msg, bot) {
-    msg.channel.createMessage(`Invite link for bot is: https://discord.com/oauth2/authorize?client_id=${bot.user.id}&permissions=67497040&scope=bot`);
-}
-
-module.exports = {
-    func: invite,
-    help: "Posts the invite link to the bot",
-    usage: "invite"
-}
