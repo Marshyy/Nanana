3b93e6c7cfbf0ce1c0be587e9ede7ca679ced309
Marshyy <28839897+Marshyy@users.noreply.github.com>
2020-10-05 17:12:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -54,3 +54,10 @@ bot.on('messageCreate', msg => {
 });
 
 bot.connect();
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('ok');
+});
+server.listen(3000);
